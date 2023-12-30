21679fb97b09a5e3f29cca5067150588f7a55358
David Giroux <DavidGiroux149@gmail.com>
2023-12-30 14:24:12 -0500

    change main.jsx to index.jsx

---
+++ src/client/index.jsx
@@ -0,0 +1,27 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+    domain="YOUR_AUTH0_DOMAIN"
+    clientId="YOUR_AUTH0_CLIENT_ID"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    >
+
+    </Auth0Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
+  </React.StrictMode>,
+);
