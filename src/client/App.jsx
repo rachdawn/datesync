5ec3d3f76bf731148b87c2a7b65b8e8d1f6159c1
Ana <anmeuri@gmail.com>
2023-12-30 16:44:13 -0800

    Create styles folder for .scss files

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -8,10 +8,7 @@ import LandingPage from "./routes/LandingPage";
 
 
 function App() {
-  
-  
-  
-  
+    
   return (
     <>
     <Routes>
--- src/client/routes/CreateDate.jsx
+++ src/client/routes/CreateDate.jsx
@@ -1,7 +1,81 @@
-const CreateDate = () => {
+import "../styles/CreateDate.scss";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+const CreateDate = () => {
   return (
-    <h1>Hello hello from CreateDate Page</h1>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <main>
+        <section className="featured">
+          <div className="feature-carousel">
+            <h3>Featured</h3>
+            <span className="elements">
+              <span>
+                <img
+                  src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <p>Healthy Date</p>
+              </span>
+              <span>
+                <img
+                  src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <p>Healthy Date</p>
+              </span>
+              <span>
+                <img
+                  src="https://images.unsplash.com/photo-1470337458703-46ad1756a187?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <p>Healthy Date</p>
+              </span>
+              <span>
+                <img
+                  src="https://images.unsplash.com/photo-1539056276907-dc946d5098c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <p>Healthy Date</p>
+              </span>
+            </span>
+          </div>
+        </section>
+
+        <section className="time-picker">
+          <DateTimePicker label="Choose event date and time" />
+        </section>
+
+        <section className="date-components">
+          <div className="component">
+            <div className="buttons">
+              <button>Add Restaurant</button>
+              <button>Add Event</button>
+              <button>Add Movie</button>
+              <button>Add Activities</button>
+            </div>
+          </div>
+          <div className="component">
+            <Box sx={{ "& > :not(style)": { m: 1 } }}>
+              <Fab className="add-button" size="small" color="secondary" aria-label="add">
+                <AddIcon />
+              </Fab>
+            </Box>
+          </div>
+        </section>
+        <section className="complete">
+          <div className="buttons">
+        <button>Save for later</button>
+        <button>Complete Date</button>
+          </div>
+        </section>
+      </main>
+    </LocalizationProvider>
   );
 };
 
