e3ed72c04257133df0f9945bd1cc3cd112b84ba5
Rachel Little <racheldawnlittle@gmail.com>
2023-12-31 00:55:20 -0700

    create dashboard with tab components

---
+++ src/client/components/AllDates.jsx
@@ -0,0 +1,31 @@
+import "../styles/Dashboard.scss";
+
+const AllDates = () => {
+  return (
+    <>
+      <article>
+        <img
+          src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <h3>Hello from ALL your dates! 👀</h3>
+      </article>
+      <article>
+        <img
+          src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <h3>Hello from ALL your dates! 👀</h3>
+      </article>      
+      <article>
+        <img
+          src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <h3>Hello from ALL your dates! 👀</h3>
+      </article>
+    </>
+  );
+};
+
+export default AllDates;
