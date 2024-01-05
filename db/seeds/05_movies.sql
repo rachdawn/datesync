2a6e18586824cad6888e15f015d4860728b96394
Ana <anmeuri@gmail.com>
2024-01-04 21:14:07 -0800

    Adjustments on db schema and seeds

--- db/seeds/05_movies.sql
+++ db/seeds/05_movies.sql
@@ -1,5 +1,5 @@
 INSERT INTO movies (category_id, component_id, movie_title, start_time, duration, movie_theatre, synopsis, photo_url, address, movie_url)
-VALUES (1, 1, 'Inception', '18:00:00', 150, 'Toronto Cinema', 'A mind-bending thriller...', 'http://example.com/inception.jpg', '123 Main St, Toronto', 'http://example.com/inception'),
-(1, 3, 'Spider-Man: No Way Home', '20:00:00', 140, 'Downtown Theater', 'The multiverse collides...', 'http://example.com/spiderman.jpg', '456 King St, Toronto', 'http://example.com/spiderman'),
-(1, 5, 'Dune', '16:30:00', 155, 'Cityplex Cinema', 'A sci-fi epic on a desert planet...', 'http://example.com/dune.jpg', '789 Queen St, Toronto', 'http://example.com/dune'),
-(1, 13, 'The Matrix Resurrections', '21:30:00', 135, 'Metroplex Theater', 'Neo returns to the matrix...', 'http://example.com/matrix.jpg', '101 Bay St, Toronto', 'http://example.com/matrix')
+VALUES (1, 1, 'Inception', '18:00:00', 150, 'Toronto Cinema', 'A mind-bending thriller...', 'https://i.ebayimg.com/images/g/N84AAOSwYGFUwoNk/s-l1600.jpg', '123 Main St, Toronto', 'http://example.com/inception'),
+(1, 3, 'Spider-Man: No Way Home', '20:00:00', 140, 'Downtown Theater', 'The multiverse collides...', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-qQ5pElti_H1lJasrNw4zKJwnEDJwupfyOw&usqp=CAU', '456 King St, Toronto', 'http://example.com/spiderman'),
+(1, 5, 'Dune', '16:30:00', 155, 'Cityplex Cinema', 'A sci-fi epic on a desert planet...', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFxNu9ti06baJREtdE5vja7WoxpSJPrV75sg&usqp=CAU', '789 Queen St, Toronto', 'http://example.com/dune'),
+(1, 13, 'The Matrix Resurrections', '21:30:00', 135, 'Metroplex Theater', 'Neo returns to the matrix...', 'https://i.etsystatic.com/15554723/r/il/dcf13a/1531064260/il_fullxfull.1531064260_jltb.jpg', '101 Bay St, Toronto', 'http://example.com/matrix')
