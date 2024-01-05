20b10aa13481751f3de3a994e096cc9bcfb3a5aa
David Giroux <DavidGiroux149@gmail.com>
2024-01-05 15:35:53 -0500

    add db queries api route

--- src/server/main.js
+++ src/server/main.js
@@ -2,12 +2,13 @@ import express from "express";
 import ViteExpress from "vite-express";
 import 'dotenv/config';
 import externalApiRoutes from "./routes/external-api.js";
+import dbQueriesApiRoutes from "./routes/db-query-api.js"
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', externalApiRoutes);
+app.use('/api', externalApiRoutes, dbQueriesApiRoutes);
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
