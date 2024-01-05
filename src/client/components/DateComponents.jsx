414e7ecb548438c153924baa8706ff861b98d849
Ana <anmeuri@gmail.com>
2024-01-04 21:11:27 -0800

    Create function component to render each date_component

---
+++ src/client/components/DateComponents.jsx
@@ -0,0 +1,53 @@
+const DateComponents = ({ dates }) => (
+  <div className="date-group">
+    {dates.map(
+      (item, index) =>
+        item.movie_title && (
+          <article key={index}>
+            <img src={item.movie_photo_url} alt="" />
+            <div>
+              <p>{item.movie_title}</p>
+              <p>{item.movie_theatre}</p>
+              <p>{item.movie_address}</p>
+            </div>
+          </article>
+        )
+    )}
+    {dates.map(
+      (item, index) =>
+        item.restaurant_name && (
+          <article key={index}>
+            <img src={item.restaurant_photo_url} alt="" />
+            <div>
+              <p>{item.restaurant_name}</p>
+              <p>{item.restaurant_address}</p>
+              <p>{item.restaurant_website_url}</p>
+            </div>
+          </article>
+        )
+    )}
+    {dates.map(
+      (item, index) =>
+        item.event_title && (
+          <article key={index}>
+            <img src={item.event_photo_url} alt="" />
+            <div>
+              <p>{item.event_title}</p>
+              <p>{item.event_description}</p>
+              <p>{item.event_url}</p>
+            </div>
+          </article>
+        )
+    )}
+    {dates.map(
+      (item, index) =>
+        item.activity_type && (
+          <article key={index}>
+            <h1>Activities</h1>
+          </article>
+        )
+    )}
+  </div>
+);
+
+export default DateComponents;
