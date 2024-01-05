091b74e2e655fdc070ec9bc8b9e27bd48993db26
David Giroux <DavidGiroux149@gmail.com>
2024-01-05 15:19:48 -0500

    refactor main.js server file and add use of routes

--- src/server/main.js
+++ src/server/main.js
@@ -1,40 +1,13 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import axios from "axios";
 import 'dotenv/config';
-
-import getDateComponents from '../../db/queries/dates.js';
+import externalApiRoutes from "./routes/external-api.js";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
-
 app.use(express.json());
 
-app.get('/local', async (req, res) => {
-  try {
-    const query = 'Beaches'
-    const location = 'Vancouver'
-    const apiUrl = `https://serpapi.com/search.json?engine=google_local&q=${query}&location=${location}&google_domain=google.com&api_key=${process.env.API_KEY}`; 
-    const response = await axios.get(apiUrl);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
-});
-
-app.get('/dates', async (req, res) => {
-  getDateComponents()
-  .then((rows) => {
-    res.json(rows);
-  })
-  .catch(error => {
-    console.error(error);
-    res.send(error);
-  })
-});
+app.use('/api', externalApiRoutes);
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
