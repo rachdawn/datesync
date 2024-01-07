d1e95c865591328fe8cef2b6964f9967b2fdd758
Rachel Little <racheldawnlittle@gmail.com>
2024-01-07 15:08:17 -0700

    corrected merge files

--- src/client/components/dashboard-tabs/PastDates.jsx
+++ src/client/components/dashboard-tabs/PastDates.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import DateComponents from "./DateComponents";
+import DateComponents from "../DateComponents";
 
 const PastDates = () => {
   const [dates, setDates] = useState([]);
