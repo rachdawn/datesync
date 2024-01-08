c7d1671fa91cf12beb658cea739413453fc246f9
David Giroux <DavidGiroux149@gmail.com>
2024-01-08 12:43:21 -0500

    fix syntax issue with topnavbar import in the call back page

--- src/client/components/auth/pages/callback-page.jsx
+++ src/client/components/auth/pages/callback-page.jsx
@@ -1,4 +1,4 @@
-import { TopNavBar } from '../../TopNavBar'
+import TopNavBar from "../../TopNavBar";
 
 // The callback-page.js component will only render the navigation bar and an empty content container to help us create a smooth transition between a route with no content, /callback, to a route with content, such as the /profile page.
 
