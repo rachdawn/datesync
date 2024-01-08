540115e167d69d95666fa665b99ed65f92d3fd97
David Giroux <DavidGiroux149@gmail.com>
2024-01-08 15:37:07 -0500

    add import pageloader to app component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import TopNavBar from "./components/TopNavBar";
@@ -5,8 +6,19 @@ import { CallbackPage } from "./components/auth/pages/callback-page";
 import CreateDate from "./routes/CreateDate";
 import Dashboard from "./routes/Dashboard";
 import LandingPage from "./routes/LandingPage";
+import { PageLoader } from "./components/PageLoader";
 
 function App() {
+  // While the React SDK is loading, the PageLoader component renders, which shows up an animation. Log out and log back in to see this in action. No more UI flashing should happen:
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="page-layout">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <>
