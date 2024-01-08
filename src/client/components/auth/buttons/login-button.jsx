8b568861ffc25de1b7a4bd7b0b8ce1ef09b08cc3
David Giroux <DavidGiroux149@gmail.com>
2024-01-08 13:54:54 -0500

    add login button component

---
+++ src/client/components/auth/buttons/login-button.jsx
@@ -0,0 +1,22 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  // The loginWithRedirect() method from the Auth0Context performs a redirect to the Auth0 /authorize endpoint to kickstart the authentication process. We can pass a configuration object to this method to customize the login experience.
+
+  // By setting up the value of appState.returnTo to /profile, we are telling the Auth0 React SDK the following: When our users log in with Auth0 and return to our React application, take them from the default callback URL path, /callback, to the "Profile" page, /profile:
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile",
+      },
+    });
+  };
+
+  return (
+    <button className="button__login" onClick={handleLogin}>
+      Log In
+    </button>
+  );
+};
