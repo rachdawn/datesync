c488389bc7e2553e36c3d71c3e590a11da3e9ed2
David Giroux <DavidGiroux149@gmail.com>
2024-01-08 22:52:00 -0500

    add public and protected routes to express server

--- src/server/main.js
+++ src/server/main.js
@@ -1,9 +1,11 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import 'dotenv/config';
-import morgan from 'morgan';
+import "dotenv/config";
+import morgan from "morgan";
 import externalApiRoutes from "./routes/external-api.js";
 import dbQueriesApiRoutes from "./routes/db-query-api.js"
+import publicRoutes from "./routes/public-routes.js"
+import protectedRoutes from "./routes/protected-routes.js"
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(express.json());
 // Use of Morgan for logging incoming requests
 app.use(morgan('dev'));
 
-app.use('/api', externalApiRoutes, dbQueriesApiRoutes);
+app.use('/api', externalApiRoutes, dbQueriesApiRoutes, publicRoutes, protectedRoutes );
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening on port ${PORT}...`),
