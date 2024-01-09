d9f3107ad2fccda3fcfe7e07ce831555b3b680a7
Ana <anmeuri@gmail.com>
2024-01-08 17:44:00 -0800

    Add featured dates section to landing page

--- src/client/routes/LandingPage.jsx
+++ src/client/routes/LandingPage.jsx
@@ -1,14 +1,13 @@
-import TestActivitiesApi from "../components/TestActivitiesApi";
-import TestEventsApi from "../components/TestEventsApi";
-import TestMoviesApi from "../components/TestMoviesApi";
-import TestRestaurantsApi from "../components/TestRestaurantsAPI";
+// import TestActivitiesApi from "../components/TestActivitiesApi";
+// import TestEventsApi from "../components/TestEventsApi";
+// import TestMoviesApi from "../components/TestMoviesApi";
+// import TestRestaurantsApi from "../components/TestRestaurantsAPI";
 
-import FeatureDates from '../components/FeatureDates'
+import FeatureDates from "../components/FeatureDates";
 
 import "../styles/LandingPage.scss";
 
 const LandingPage = () => {
-
   const featureDates = [
     {
       src: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -41,21 +40,35 @@ const LandingPage = () => {
       <section className="hero">
         <div className="hero-inner">
           <h1>DateSync</h1>
-          <h2>The best place to plan your Perfect Dates</h2>
-          <a href="" className="btn btn-primary">
+          <h3>The best place to plan your Perfect Dates</h3>
+          <a href="" className="btn btn-light">
             Sign Up for free
           </a>
         </div>
       </section>
-      <main>
-        {/* <div className="cards">
-          {featureDates.map((date, index) => (
-            <div className="feature-card" key={index}>
-              <FeatureDates date={date} />
-            </div>
-          ))}
-        </div> */}
+      <main className="featured">
+        <div className="feature-carousel">
+          <div className="feature-title">
+            <h2>Feature Dates</h2>
+          </div>
+          <div className="cards">
+            {featureDates.map((date, index) => (
+              <div className="feature-card" key={index}>
+                <FeatureDates date={date} />
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
+      <section>
+        <a className="btn btn-secondary" href="">
+          Create account
+        </a>
+
+        <a className="btn btn-secondary" href="">
+          Try without account
+        </a>
+      </section>
       {/* <TestRestaurantsApi />
     <TestActivitiesApi />
     <TestEventsApi />
