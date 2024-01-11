99d0e191e9fa0d4856838274133b3b9ac7cf7104
David Giroux <DavidGiroux149@gmail.com>
2024-01-11 18:15:40 -0500

    add jsx to target response data for client rendering

--- src/client/components/result-accordions/ActivitiesAccordion.jsx
+++ src/client/components/result-accordions/ActivitiesAccordion.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function ActivitiesAccordion({ activity }) {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -15,28 +15,28 @@ export default function ActivitiesAccordion({ activity }) {
   return (
     <div className="result">
       <Accordion
-        expanded={expanded === "panel1"}
-        onChange={handleChange("panel1")}
+        expanded={expanded === activity.id}
+        onChange={handleChange(activity.id)}
       >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
+          aria-controls={`${activity.id}-content`}
+          id={`${activity.id}-header`}
         >
           <div className="result-card">
             <div className="card-mobile">
               <div>
-                <img className="result-img" src={activity.photo_url} alt="" />
+                <img className="result-img" src={activity.thumbnail} alt={activity.title} />
               </div>
               <div className="info">
-                <h3>{activity.activity}</h3>
+                <h3>{activity.title}</h3>
                 <p>{activity.description}</p>
               </div>
             </div>
             <div>
               <div className="actions">
                 <button>Add to Date</button>
-                <a href={activity.activity_url} target="_blank">
+                <a href={activity.website} target="_blank" rel="noopener noreferrer">
                   <button>Visit Website</button>
                 </a>
               </div>
@@ -45,7 +45,7 @@ export default function ActivitiesAccordion({ activity }) {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            {activity.location_name} -- {activity.address}
+            {activity.title} -- {activity.address}
           </Typography>
         </AccordionDetails>
       </Accordion>
