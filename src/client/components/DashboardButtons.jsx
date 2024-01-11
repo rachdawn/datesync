777ef85589c189999523e66f1c9ea53a1f7cbbcc
Ana <anmeuri@gmail.com>
2024-01-10 20:21:25 -0800

    Refactor code to make use of useDates hook

--- src/client/components/DashboardButtons.jsx
+++ src/client/components/DashboardButtons.jsx
@@ -1,12 +1,19 @@
-const DashboardButtons = ({ dateInfo }) => {
+const DashboardButtons = ({ dateInfo, shareDate }) => {
   const today = new Date().toISOString();
+
+  const handleShare = (id) => shareDate(id)
 
   return (
     <>
-      {(dateInfo.is_draft) && (
-         <span className="date-component-buttons"> 
-          <h5><span className="badge rounded-pill text-bg-danger">Draft</span></h5>
-          <a href="" className="btn btn-secondary btn-dashboard">
+      {dateInfo.is_draft && (
+        <span className="date-component-buttons">
+          <h5>
+            <span className="badge rounded-pill text-bg-danger">Draft</span>
+          </h5>
+          <a
+            onClick={() => handleShare(dateInfo.date_id)}
+            className="btn btn-secondary btn-dashboard"
+          >
             Share
           </a>
           <a href="" className="btn btn-secondary btn-dashboard">
@@ -18,9 +25,9 @@ const DashboardButtons = ({ dateInfo }) => {
         </span>
       )}
 
-      {(dateInfo.scheduled_date < today) && (
+      {dateInfo.scheduled_date < today && (
         <span className="date-component-buttons">
-          <a href="" className="btn btn-secondary btn-dashboard">
+          <a className="btn btn-secondary btn-dashboard">
             Redo Date
           </a>
           <a href="" className="btn btn-secondary btn-dashboard">
@@ -29,12 +36,15 @@ const DashboardButtons = ({ dateInfo }) => {
         </span>
       )}
 
-      {(dateInfo.scheduled_date > today) && (
+      {dateInfo.scheduled_date > today && (
         <span className="date-component-buttons">
           <a href="" className="btn btn-secondary btn-dashboard">
             Edit
           </a>
-          <a href="" className="btn btn-secondary btn-dashboard">
+          <a
+            onClick={() => handleShare(dateInfo.date_id)}
+            className="btn btn-secondary btn-dashboard"
+          >
             Share
           </a>
           <a href="" className="btn btn-secondary btn-dashboard">
