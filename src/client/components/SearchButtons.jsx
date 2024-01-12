ea82df8c5500acf773e36c0c81c7a52604132002
David Giroux <DavidGiroux149@gmail.com>
2024-01-12 15:14:27 -0500

    syntax change

--- src/client/components/SearchButtons.jsx
+++ src/client/components/SearchButtons.jsx
@@ -4,13 +4,15 @@ import RestaurantsModal from "../components/date-modals/RestaurantsModal";
 import EventsModal from "../components/date-modals/EventsModal";
 
 // This component serves to render the container with the search buttons in the CreateDate component:
-const SearchButtons = ({ coordinates, cityString }) => (
+const SearchButtons = ({ coordinates, cityString }) => {
+  return(
   <div className="buttons">
     <RestaurantsModal coordinates={coordinates} />
     <EventsModal cityString={cityString} />
     <MoviesModal cityString={cityString} />
     <ActivitiesModal coordinates={coordinates} />
   </div>
-);
+  );
+};
 
 export default SearchButtons;
