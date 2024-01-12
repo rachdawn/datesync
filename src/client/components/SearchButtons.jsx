2da0274775f013e2a0d1b9406c77a415576924b9
David Giroux <DavidGiroux149@gmail.com>
2024-01-12 12:53:03 -0500

    add searchbuttons component for the create date page

---
+++ src/client/components/SearchButtons.jsx
@@ -0,0 +1,16 @@
+import ActivitiesModal from "../components/date-modals/ActivitiesModal";
+import MoviesModal from "../components/date-modals/MoviesModal";
+import RestaurantsModal from "../components/date-modals/RestaurantsModal";
+import EventsModal from "../components/date-modals/EventsModal";
+
+// This component serves to render the container with the search buttons in the CreateDate component:
+const SearchButtons = ({ coordinates, cityString }) => (
+  <div className="buttons">
+    <RestaurantsModal coordinates={coordinates} />
+    <EventsModal cityString={cityString} />
+    <MoviesModal cityString={cityString} />
+    <ActivitiesModal coordinates={coordinates} />
+  </div>
+);
+
+export default SearchButtons;
