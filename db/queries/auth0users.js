9e6092ff45a98be2df1bc85b8daaeaf8efab571a
David Giroux <DavidGiroux149@gmail.com>
2024-01-11 23:57:24 -0500

    add users queries for adding user to db and checking if user exists already in db

---
+++ db/queries/auth0users.js
@@ -0,0 +1,71 @@
+import db from "../connection.js";
+
+// Following code consists of db queries:
+
+const getUsers = async () => {
+  return db.query('SELECT * FROM users;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+// This function will retrieve a user from the database based on the username. This is useful for the login process where you need to check if the user exists and then verify the password:
+const getUserByUsername = async (username) => {
+  const queryString = `SELECT * FROM users WHERE username = $1;`;
+  const values = [username];
+
+
+  return db.query(queryString, values)
+    .then(data => {
+      if (data.rows.length === 0) {
+        return null;
+      }
+      // Return the first user matching the username
+      return data.rows[0];
+    })
+    .catch(err => console.log('Error executing query', err.message));
+};
+
+const getUserWithId = async function(id) {
+  const queryString = `SELECT * FROM users WHERE id = $1;`;
+  const values = [id];
+
+  return db.query(queryString, values)
+    .then((data) => {
+      if (data.rows.length === 0) {
+        console.log(`No user found with id: ${id}`);
+        return null;
+      }
+      // client.end();
+      return data.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+// Adding users to our db with the help of auth0 retrieved profile info:
+const addUser = async (user) => {
+  const queryString = `INSERT INTO users (email, given_name, family_name) VALUES ($1, $2, $3 ) RETURNING *`;
+  // Assuming location is part of the user object; otherwise, set a default
+  const values = [user.email, user.given_name, user.family_name];
+
+  console.log("Inserting into database:", values);
+
+  return db.query(queryString, values)
+  .then(data => {
+    console.log("Inserted user:", data.rows[0]);
+    return data.rows[0];
+  })
+  .catch(err => console.log('Error executing query', err.message));
+};
+
+// Handles cases to check if the user exists in our db:
+const checkUserExists = async (email) => {
+  const query = 'SELECT * FROM users WHERE email = $1';
+  const result = await db.query(query, [email]);
+  return result.rows.length > 0;
+};
+
+
+export { getUsers, getUserByUsername, getUserWithId, addUser, checkUserExists };
