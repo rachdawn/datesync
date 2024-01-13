5584eaa17554fc156d2ff98e6d4143fdd6fc92c4
David Giroux <DavidGiroux149@gmail.com>
2024-01-13 12:03:07 -0500

    add function for when a user selects a specific showtime which would be called up from the moviesmodal to be handled

--- src/client/components/Showtimes.jsx
+++ src/client/components/Showtimes.jsx
@@ -1,6 +1,25 @@
 import { Typography, ButtonGroup, Button, Card, CardContent, CardActions, Grid } from '@mui/material';
 
-const Showtimes = ({ showtimes, selectedMovie, handleBackToMovies }) => {
+const Showtimes = ({ showtimes, selectedMovie, handleBackToMovies, onShowtimeSelect }) => {
+
+  // This function is called when a user selects a specific showtime. It builds an object (selectedShowtime) containing details about the selected movie and the chosen showtime (including showtime info). It also calls on onShowtimeSelct to handle selectedShowtime in the parent component:
+  const handleShowtimeSelect = (day, theater, time) => {
+    const selectedShowtime = {
+      movie: selectedMovie,
+      showtime: {
+        day: day.day,
+        date: day.date,
+        time: time,
+        theater: {
+          name: theater.name,
+          address: theater.address
+        }
+      }
+    };
+
+    onShowtimeSelect(selectedShowtime);
+  };
+
   return (
     <>
       <Typography variant="h4" gutterBottom>
@@ -24,7 +43,7 @@ const Showtimes = ({ showtimes, selectedMovie, handleBackToMovies }) => {
                       {theater.showing.map((show, showIndex) => (
                         <ButtonGroup key={showIndex} variant="text" aria-label="text button group" fullWidth>
                           {show.time.map((time, timeIndex) => (
-                            <Button key={timeIndex}>
+                            <Button key={timeIndex} onClick={() => handleShowtimeSelect(day, theater, time)}>
                               {time} ({show.type})
                             </Button>
                           ))}
