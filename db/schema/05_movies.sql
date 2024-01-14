6e2de5c6050995fe162b0478f2ad3d1c77bab520
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 13:31:07 -0500

    change start_time, duration and movie_url to nullable

--- db/schema/05_movies.sql
+++ db/schema/05_movies.sql
@@ -4,11 +4,11 @@ CREATE TABLE "movies"(
     "category_id" INTEGER REFERENCES categories(id) ON DELETE CASCADE,
     "component_id" INTEGER REFERENCES date_components(id) ON DELETE CASCADE,
     "movie_title" VARCHAR(255) NOT NULL,
-    "start_time" TIME(0) WITH TIME ZONE NOT NULL,
-    "duration" INTEGER NOT NULL,
+    "start_time" TIME(0) WITH TIME ZONE NULL,
+    "duration" INTEGER NULL,
     "movie_theatre" VARCHAR(255) NOT NULL,
     "synopsis" TEXT NOT NULL,
     "photo_url" TEXT NOT NULL,
     "address" VARCHAR(255) NOT NULL,
-    "movie_url" VARCHAR(255) NOT NULL
+    "movie_url" VARCHAR(255) NULL
 );
