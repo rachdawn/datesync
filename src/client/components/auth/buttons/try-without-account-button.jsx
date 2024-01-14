61225c9fed3130911c492f9aba23d20937f93c28
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 17:10:23 -0500

    change logic so that try without account leads to create date page without needing to log in or be authenticated

--- src/client/components/auth/buttons/try-without-account-button.jsx
+++ src/client/components/auth/buttons/try-without-account-button.jsx
@@ -1,28 +1,18 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 const TryWithoutAccountButton = ({ className }) => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-
-  if (isAuthenticated) return null;
-
-  const handleTryWithoutAccount = async () => {
-    await loginWithRedirect({
-      appState: {
-        returnTo: "/create-date",
-      },
-    });
-  };
 
   return (
-    <Button
-      variant="contained"
-      color="secondary" 
-      onClick={handleTryWithoutAccount}
-      className={className}
-    >
-      Try without account
-    </Button>
+    <Link to="/create-date" style={{ textDecoration: "none" }}>
+      <Button
+        variant="contained"
+        color="secondary" 
+        className={className}
+      >
+        Try without account
+      </Button>
+    </Link>
   );
 };
 
