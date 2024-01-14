36b65e9af756507cdf3c10ed6cc5fa91a168ada4
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 13:44:40 -0500

    change times to text and other attributes to nullable,

--- db/schema/07_events.sql
+++ db/schema/07_events.sql
@@ -5,12 +5,12 @@ CREATE TABLE "events"(
     "component_id" INTEGER REFERENCES date_components(id) ON DELETE CASCADE,
     "title" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
-    "start_date_time" TIMESTAMP(0) WITH TIME ZONE NULL,
-    "end_date_time" TIMESTAMP(0) WITH TIME ZONE NULL,
+    "start_date_time" TEXT NULL,
+    "end_date_time" TEXT NULL,
     "location_name" VARCHAR(255) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
-    "event_type" VARCHAR(255) NOT NULL,
-    "price" INTEGER NOT NULL,
-    "event_url" VARCHAR(255) NOT NULL,
+    "event_type" VARCHAR(255) NULL,
+    "price" INTEGER NULL,
+    "event_url" VARCHAR(255) NULL,
     "photo_url" TEXT NOT NULL
 );
