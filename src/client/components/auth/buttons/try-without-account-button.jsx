5830159b924c2a55eaf4e3f8c5344a85959ee817
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 17:18:42 -0500

    add logic so that logged in or authenticated user will not see the try for free button upon visit on landing page

--- src/client/components/auth/buttons/try-without-account-button.jsx
+++ src/client/components/auth/buttons/try-without-account-button.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const TryWithoutAccountButton = ({ className }) => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+  // This only shows the button if the user is not authenticated:
+  if (isAuthenticated) return null;
 
   return (
     <Link to="/create-date" style={{ textDecoration: "none" }}>
