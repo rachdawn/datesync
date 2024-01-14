015a08be434d46150949574bff9b1537244ec733
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 13:45:50 -0500

    change to text and nullable

--- db/schema/08_activities.sql
+++ db/schema/08_activities.sql
@@ -3,12 +3,12 @@ CREATE TABLE "activities"(
     "id" SERIAL PRIMARY KEY NOT NULL,
     "category_id" INTEGER REFERENCES categories(id) ON DELETE CASCADE,
     "component_id" INTEGER REFERENCES date_components(id) ON DELETE CASCADE,
-    "activity_type" VARCHAR(255) NOT NULL,
+    "activity_type" VARCHAR(255) NULL,
     "location_name" VARCHAR(255) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
     "description" TEXT NULL,
     "photo_url" TEXT NULL,
-    "activity_url" VARCHAR(255) NULL
+    "activity_url" TEXT NULL
 );
 
 
