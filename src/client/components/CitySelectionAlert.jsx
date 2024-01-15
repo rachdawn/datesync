62babba102dc4cefdacd14ca13a171f5d8509eb5
David Giroux <DavidGiroux149@gmail.com>
2024-01-14 23:12:28 -0500

    add styling to alert notification

--- src/client/components/CitySelectionAlert.jsx
+++ src/client/components/CitySelectionAlert.jsx
@@ -3,11 +3,26 @@ import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 const CitySelectionAlert = ({ onClose }) => {
   return (
+    <div className="alert-notification">
     <ClickAwayListener onClickAway={onClose}>
-      <Alert severity="info">
+    <Alert 
+      severity="warning" 
+      variant="outlined" 
+      sx={{ 
+        borderColor: '#662d91', 
+        color: '#662d91', 
+        '& .MuiAlert-icon': {
+          color: '#662d91', 
+        },
+        '& .MuiAlert-message': {
+          color: '#662d91', 
+        }
+      }}
+      >
         Please select a city first.
       </Alert>
     </ClickAwayListener>
+    </div>
   );
 };
 
