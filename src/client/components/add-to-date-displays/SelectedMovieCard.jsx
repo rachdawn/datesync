3b6f69f0964b551159b04b8fe9d9739ee0b974b5
Rachel Little <racheldawnlittle@gmail.com>
2024-01-15 14:10:35 -0700

    style date component cards

--- src/client/components/add-to-date-displays/SelectedMovieCard.jsx
+++ src/client/components/add-to-date-displays/SelectedMovieCard.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteIcon from "/src/client/assets/delete-icon.svg";
+import "/src/client/styles/DateComponentCards.scss";
 
 const SelectedMovieCard = ({ movie }) => {
   // The movie object is nested within another object thus the following destructuring:
-  const { movie: { title, image, details }, showtime } = movie;
+  const {
+    movie: { title, image, details },
+    showtime,
+  } = movie;
   const { day, date, time, theater } = showtime;
 
   return (
-    <Card className="selected-movie-card">
-      <CardContent>
-        <Typography variant="h5" component="div">
+    <Card className="date-card">
+      <button className="delete-button">
+        <img src={DeleteIcon} alt="Remove Selection" />
+      </button>
+      <CardMedia
+        component="img"
+        className="date-card-img"
+        image={image}
+        alt={title}
+      />
+      <CardContent className="card-content">
+        <Typography className="date-title" variant="h5" component="div">
           {title}
         </Typography>
-        <img src={image} alt={title} style={{ maxWidth: '100%', height: 'auto' }} />
-
-        <Typography variant="body1" color="text.secondary">
-          {details}
-        </Typography>
-
-        <Typography variant="body2" color="text.secondary">
-          Selected Showtime: {time} on {day}, {date}
-        </Typography>
-
-        <Typography variant="body2" color="text.secondary">
-          At: {theater.name}
-        </Typography>
-
-        <Typography variant="body2" color="text.secondary">
-          Address: {theater.address}
+        <Typography className="date-type" variant="body2">
+          Selected Showtime: {day}, {date} at {time}
         </Typography>
       </CardContent>
+      <Accordion className="card-menu">
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+          className="card-menu-title"
+        >
+          Movie Details
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography className="date-type" variant="body2">
+            {details}
+          </Typography>
+          <hr />
+          <Typography variant="body2">{theater.name}</Typography>
+          <Typography variant="body2">{theater.address}</Typography>
+        </AccordionDetails>
+      </Accordion>
     </Card>
   );
 };
