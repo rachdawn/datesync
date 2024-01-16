26ada856cf6972b6987840c99ebcf7578ce5bce6
David Giroux <DavidGiroux149@gmail.com>
2024-01-16 18:53:27 -0500

    remove accordion feature as it will be unneeded for carousel feature

--- src/client/components/FeatureDates.jsx
+++ src/client/components/FeatureDates.jsx
@@ -1,20 +1,15 @@
-import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "../styles/FeatureDates.scss";
 
 export default function FeatureDates({ date }) {
   return (
     <Card className="feature-card">
       <CardActionArea>
-        <CardMedia component="img" height="140" image={date.src} alt="date" />
+        <CardMedia component="img" height="340" image={date.src} alt="date" />
         <CardContent className="feature-desktop">
           <Typography
             className="date-title"
@@ -24,23 +19,10 @@ export default function FeatureDates({ date }) {
           >
             {date.title}
           </Typography>
-          <Typography variant="body" color="text.secondary">
+          <Typography variant="body" color="text.secondary" className="description-text">
             {date.description}
           </Typography>
         </CardContent>
-
-        <Accordion className="feature-mobile">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className="date-title">{date.title}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>{date.description}</Typography>
-          </AccordionDetails>
-        </Accordion>
       </CardActionArea>
     </Card>
   );
