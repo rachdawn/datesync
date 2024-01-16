e3d81012c860bc647bafae1aa64d54ce602fa4b7
David Giroux <DavidGiroux149@gmail.com>
2024-01-15 20:46:26 -0500

    add new mock data for featured dates

--- src/client/components/mockFeatureDates.jsx
+++ src/client/components/mockFeatureDates.jsx
@@ -1,27 +1,45 @@
+import Mediterranean from '../assets/Mediterranean.jpeg';
+import AquamanMovie from '../assets/AquamanMovie.png';
+import IceRinkMountRoyalPark from '../assets/IceRinkMountRoyalPark.png';
+import IglooFest from '../assets/IglooFest.png';
+import Clockwork from '../assets/Clockwork.png'
+
+
 const mockFeatureDates = [
   {
-    src: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Healthy date",
-    description:
-      "Nourish love with a delightful, nutritious dinner, a romantic stroll, and shared laughter under starlight.",
+    src: Mediterranean,
+    title: "Hydra Estiatorio",
+    type: "Greek Restaurant",
+    description: "Hydra offers quality Mediterranean dishes that will take you to the aquamarine waters of Greece.",
+    website: "https://www.hydravancouver.com/",
   },
   {
-    src: "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Fancy lunch",
-    description:
-      "Savor exquisite flavors in a chic ambiance with our gourmet lunch, a sophisticated culinary indulgence awaits.",
+    src: AquamanMovie,
+    title: "Aquaman and the Lost Kingdom",
+    type: "Movie",
+    description: "Aquaman forges an unlikely alliance with his brother, Orm, to save the world from Black Manta.",
+    website: "https://www.youtube.com/watch?v=FV3bqvOHRQo",
   },
   {
-    src: "https://images.unsplash.com/photo-1470337458703-46ad1756a187?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Late-night drinks",
-    description:
-      "Sip under the stars in a cozy ambiance, where late-night drinks spark laughter, intimacy, and connection.",
+    src: IceRinkMountRoyalPark,
+    title: "Mont Royal Park",
+    type: "Ice Rink Activity",
+    description: "Experience the magic of winter by ice skating on Mount Royal's picturesque outdoor rink",
+    website: "https://www.lemontroyal.qc.ca/en",
   },
   {
-    src: "https://images.unsplash.com/photo-1539056276907-dc946d5098c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Friends Dinner",
-    description:
-      "Relish a delightful evening with friends over a homemade feast, laughter, and shared memories together.",
+    src: IglooFest,
+    title: "Igloofest",
+    type: "Music Festival",
+    description: "Join the winter celebration at Montreal's Igloofest, an outdoor electronic music festival",
+    website: "https://www.igloofest.ca/",
+  },
+  {
+    src: Clockwork,
+    title: "Clockwork Champagne & Cocktails",
+    type: "Cocktail Bar",
+    description: "Step into Clockwork Champagne & Cocktails, Toronto's premier destination for an elegant evening.",
+    website: "https://www.clockworktoronto.com/",
   },
 ];
 
