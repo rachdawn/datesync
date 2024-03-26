4f7b5dac739bdc3e0c80dfd95680c66ef5e009b4
DavidGir <davidgiroux149@gmail.com>
2024-03-26 11:54:45 -0400

    Add SSL certificate configuration to database connection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,14 @@
 // Import the entire pg module
 import pg from 'pg';
+// Import the fs module
+import fs from 'fs';
+// Import the dotenv module
+import "dotenv/config";
 // Destructure Pool from the imported module
 const { Pool } = pg;
+
+// Specify the path to the SSL certificate:
+const sslCertPath = "./aws_rds_ssl_certificate/ca-central-1-bundle.pem"
 
 // PG database client/connection setup
 const dbParams = {
@@ -9,7 +16,11 @@ const dbParams = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(sslCertPath).toString()
+  }
 };
 
 const db = new Pool(dbParams);
